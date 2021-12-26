1a0223512f863c17274580320608ce796ae5cf99
xlong <34159358+coxlong@users.noreply.github.com>
2021-12-26 13:47:43 +0800

    Update diy-part1.sh
    添加passwall

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,5 @@
 
 
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+
+echo 'src-git openwrtpasswall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
