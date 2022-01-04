7002a1340abc06916a861e7638d8b55e9590d341
xlong <34159358+coxlong@users.noreply.github.com>
2022-01-04 13:41:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >>feeds.conf.default
 
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-
-echo 'src-git openwrtpasswall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
